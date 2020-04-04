246c770792e751db3c432298b67e3d5039cd0772
Satyam-code143 <satyamdodmani555@gmail.com>
2020-04-04 16:01:19 +0530

    fixed colour of tab issue

--- src/HomePage/NavBar.js
+++ src/HomePage/NavBar.js
@@ -2,27 +2,43 @@ import React from 'react';
 import { NavLink} from 'react-router-dom';
 import './Styles/NavBarStyle.css';
 
-const NavBar=()=>{
-return(
-  <div className='NavBarStyle'>
-    <nav>
-      <div className="logo">
-        <h1>
-          Manage Mart
-        </h1>
-      </div>
+class NavBar extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      isButtonSelected: 'home',
+      
+    };
+  }
 
-      <div className="menu">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/About">About</NavLink>
-        <NavLink to="/Contact">Contact</NavLink>
-        <NavLink to="/Services">Services</NavLink>
-        <NavLink to='/Login'>Login</NavLink>
-      </div>
-    </nav>
-  </div>
+
+  setActive(){
+    this.setState({ isButtonSelected: 'nothome' });
+  }
+
+  render(){
+    return(
+      <div className='NavBarStyle'>
+        <nav>
+          <div className="logo">
+            <h1>
+              Manage Mart
+            </h1>
+          </div>
     
-)
+          <div className="menu">
+            <NavLink className={`${this.state.isButtonSelected === 'home'? 'active' : 'inactive'}`} to="/">Home</NavLink>
+            <NavLink onClick={() => this.setActive()} to="/About">About</NavLink>
+            <NavLink  onClick={() => this.setActive()} to="/Contact">Contact</NavLink>
+            <NavLink onClick={() => this.setActive()} to="/Services">Services</NavLink>
+            <NavLink onClick={() => this.setActive()} to='/Login'>Login</NavLink>
+          </div>
+        </nav>
+      </div>
+        
+    )
+  }
+
 }
 
 export default NavBar;
